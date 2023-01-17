2ee103d14b94351e17cd740b3f93386a642afde9
manishjha <manishrajkiit@gmail.com>
2023-01-18 04:18:05 +0530

    updated

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ from housing.logger import logging
 from housing.exception import HousingException
 import sys
 
-
 app=Flask(__name__)
-
 @app.route("/",methods=['GET','POST'])
 def index():
     try:
-        raise Exception("we are testing custom exceptions")
+        raise Exception("we are testing custom exceptions") 
     except Exception as e:
-        housing =HousingException(e,sys)
+        housing =HousingException(e,sys) 
         logging.info(housing.error_message)
         logging.info("HI This is my login files")
-    return "First Machine Learning Project."
+    return "CD/CI Machine Learning Project."
 
 if __name__=="__main__":
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
-
-<<<<<<< HEAD
 from setuptools import setup,find_packages
 from typing import List
 
 PROJECT_NAME='housing-predictor'
-VERSION='0.0.4'
+VERSION='0.0.5'
 AUTHOR='Manish Jha'
 DESCRIPTION='This is my first machine learning project'
 PACKAGES=['housing']
@@ -24,29 +22,12 @@ def get_requirements_list()->List[str]:
     return This function is going to return a list which contain name
     of libraries mentioned in requirements.txt file
 """
-=======
-from setuptools import setup
-from typing import List
-
-def get_requirements_list()->List[str]:
-    pass
-
-PROJECT_NAME='housing-predictor'
-VERSION='0.0.1'
-AUTHOR='Manish Jha'
-DESCRIPTION='This is my first machine learning project'
-PACKAGES=['housing']
->>>>>>> 36e7b66 (component files added)
 
 setup( 
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-<<<<<<< HEAD
     packages=find_packages(),
-=======
-    packages=PACKAGES,
->>>>>>> 36e7b66 (component files added)
     install_requires=get_requirements_list()
     )
